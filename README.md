084972d1844edb8c02ac232a777f9a8aed44ac70
Eran Hammer <eran@hammer.io>
2019-03-25 10:21:28 -0700

    Commercial copy of v1

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 `JSON.parse()` drop-in replacement with prototype poisoning protection
 
+[![Build Status](https://travis-ci.org/hapijs/bourne.svg?branch=v1-commercial)](https://travis-ci.org/hapijs/bourne)
+
+## License
+
+This version of the package requires a commercial license. You may not use, copy, or distribute it without first acquiring a commercial license from Sideway Inc. Using this software without a license is a violation of US and international law. To obtain a license, please contact [sales@sideway.com](mailto:sales@sideway.com). The open source version of this package can be found [here](https://github.com/hapijs/bourne).
+
 ## Introduction
 
 Consider this:
--- test/index.js
+++ test/index.js
@@ -16,27 +16,31 @@ describe('Bourne', () => {
 
     describe('parse()', () => {
 
-        it('parses object string', () => {
+        it('parses object string', (done) => {
 
             expect(Bourne.parse('{"a": 5, "b": 6}')).to.equal({ a: 5, b: 6 });
+            done();
         });
 
-        it('parses null string', () => {
+        it('parses null string', (done) => {
 
             expect(Bourne.parse('null')).to.equal(null);
+            done();
         });
 
-        it('parses zero string', () => {
+        it('parses zero string', (done) => {
 
             expect(Bourne.parse('0')).to.equal(0);
+            done();
         });
 
-        it('parses string string', () => {
+        it('parses string string', (done) => {
 
             expect(Bourne.parse('"x"')).to.equal('x');
+            done();
         });
 
-        it('parses object string (reviver)', () => {
+        it('parses object string (reviver)', (done) => {
 
             const reviver = (key, value) => {
 
@@ -44,9 +48,10 @@ describe('Bourne', () => {
             };
 
             expect(Bourne.parse('{"a": 5, "b": 6}', reviver)).to.equal({ a: 6, b: 7 });
+            done();
         });
 
-        it('sanitizes object string (reviver, options)', () => {
+        it('sanitizes object string (reviver, options)', (done) => {
 
             const reviver = (key, value) => {
 
@@ -54,87 +59,100 @@ describe('Bourne', () => {
             };
 
             expect(Bourne.parse('{ "a": 5, "b": 6, "__proto__": { "x": 7 } }', reviver, { protoAction: 'remove' })).to.equal({ a: 6, b: 7 });
+            done();
         });
 
-        it('sanitizes object string (options)', () => {
+        it('sanitizes object string (options)', (done) => {
 
             expect(Bourne.parse('{ "a": 5, "b": 6, "__proto__": { "x": 7 } }', { protoAction: 'remove' })).to.equal({ a: 5, b: 6 });
+            done();
         });
 
-        it('sanitizes object string (null, options)', () => {
+        it('sanitizes object string (null, options)', (done) => {
 
             expect(Bourne.parse('{ "a": 5, "b": 6, "__proto__": { "x": 7 } }', null, { protoAction: 'remove' })).to.equal({ a: 5, b: 6 });
+            done();
         });
 
-        it('sanitizes nested object string', () => {
+        it('sanitizes nested object string', (done) => {
 
             const text = '{ "a": 5, "b": 6, "__proto__": { "x": 7 }, "c": { "d": 0, "e": "text", "__proto__": { "y": 8 }, "f": { "g": 2 } } }';
             expect(Bourne.parse(text, { protoAction: 'remove' })).to.equal({ a: 5, b: 6, c: { d: 0, e: 'text', f: { g: 2 } } });
+            done();
         });
 
-        it('ignores proto property', () => {
+        it('ignores proto property', (done) => {
 
             const text = '{ "a": 5, "b": 6, "__proto__": { "x": 7 } }';
             expect(Bourne.parse(text, { protoAction: 'ignore' })).to.equal(JSON.parse(text));
+            done();
         });
 
-        it('ignores proto value', () => {
+        it('ignores proto value', (done) => {
 
             expect(Bourne.parse('{"a": 5, "b": "__proto__"}')).to.equal({ a: 5, b: '__proto__' });
+            done();
         });
 
-        it('errors on proto property', () => {
+        it('errors on proto property', (done) => {
 
             expect(() => Bourne.parse('{ "a": 5, "b": 6, "__proto__": { "x": 7 } }')).to.throw(SyntaxError);
             expect(() => Bourne.parse('{ "a": 5, "b": 6, "__proto__" : { "x": 7 } }')).to.throw(SyntaxError);
             expect(() => Bourne.parse('{ "a": 5, "b": 6, "__proto__" \n\r\t : { "x": 7 } }')).to.throw(SyntaxError);
             expect(() => Bourne.parse('{ "a": 5, "b": 6, "__proto__" \n \r \t : { "x": 7 } }')).to.throw(SyntaxError);
+            done();
         });
 
-        it('errors on proto property (null, null)', () => {
+        it('errors on proto property (null, null)', (done) => {
 
             expect(() => Bourne.parse('{ "a": 5, "b": 6, "__proto__": { "x": 7 } }', null, null)).to.throw(SyntaxError);
+            done();
         });
 
-        it('errors on proto property (explicit options)', () => {
+        it('errors on proto property (explicit options)', (done) => {
 
             expect(() => Bourne.parse('{ "a": 5, "b": 6, "__proto__": { "x": 7 } }', { protoAction: 'error' })).to.throw(SyntaxError);
+            done();
         });
 
-        it('errors on proto property (unicode)', () => {
+        it('errors on proto property (unicode)', (done) => {
 
             expect(() => Bourne.parse('{ "a": 5, "b": 6, "\\u005f_proto__": { "x": 7 } }')).to.throw(SyntaxError);
             expect(() => Bourne.parse('{ "a": 5, "b": 6, "_\\u005fp\\u0072oto__": { "x": 7 } }')).to.throw(SyntaxError);
             expect(() => Bourne.parse('{ "a": 5, "b": 6, "\\u005f\\u005f\\u0070\\u0072\\u006f\\u0074\\u006f\\u005f\\u005f": { "x": 7 } }')).to.throw(SyntaxError);
+            done();
         });
     });
 
     describe('scan()', () => {
 
-        it('sanitizes nested object string', () => {
+        it('sanitizes nested object string', (done) => {
 
             const text = '{ "a": 5, "b": 6, "__proto__": { "x": 7 }, "c": { "d": 0, "e": "text", "__proto__": { "y": 8 }, "f": { "g": 2 } } }';
             const obj = JSON.parse(text);
 
             Bourne.scan(obj, { protoAction: 'remove' });
             expect(obj).to.equal({ a: 5, b: 6, c: { d: 0, e: 'text', f: { g: 2 } } });
+            done();
         });
 
-        it('errors on proto property', () => {
+        it('errors on proto property', (done) => {
 
             const text = '{ "a": 5, "b": 6, "__proto__": { "x": 7 }, "c": { "d": 0, "e": "text", "__proto__": { "y": 8 }, "f": { "g": 2 } } }';
             const obj = JSON.parse(text);
 
             expect(() => Bourne.scan(obj)).to.throw(SyntaxError);
+            done();
         });
 
-        it('does not break when hasOwnProperty is overwritten', () => {
+        it('does not break when hasOwnProperty is overwritten', (done) => {
 
             const text = '{ "a": 5, "b": 6, "hasOwnProperty": "text", "__proto__": { "x": 7 } }';
             const obj = JSON.parse(text);
 
             Bourne.scan(obj, { protoAction: 'remove' });
             expect(obj).to.equal({ a: 5, b: 6, hasOwnProperty: 'text' });
+            done();
         });
     });
 });
