dc1f65a74963c24c0dc81dbcb11b67dc24f6f3c3
Nicolas Morel <nicolas@morel.io>
2026-07-01 11:38:47 +0200

    chore: revert extension change

--- benchmarks/no__proto__.js
+++ benchmarks/no__proto__.js
@@ -1,6 +1,6 @@
 import { Bench } from 'tinybench';
 
-import * as Bourne from '../src/index.mjs';
+import * as Bourne from '../src/index.js';
 
 const internals = {
     text: '{ "a": 5, "b": 6, "proto": { "x": 7 }, "c": { "d": 0, "e": "text", "\\u005f\\u005fproto": { "y": 8 }, "f": { "g": 2 } } }',
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 
-import * as Bourne from '../src/index.mjs';
+import * as Bourne from '../src/index.js';
 
 describe('Bourne', () => {
     describe('parse()', () => {
